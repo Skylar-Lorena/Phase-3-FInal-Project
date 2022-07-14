d9d797c511c68bacb5040a554940e4996e5ea6ad
--replace-all <--replace-all>
2022-07-14 15:31:48 +0300

    #Create trips and items tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+trip = Trip.create(name: "Example Trip")
+
+item = Item.create(name: "Example Item #1", trip_id: trip.id)
+item = Item.create(name: "Example Item #2", trip_id: trip.id)
+
+trip = Trip.create(name: "2nd Example Trip")
+
+item = Item.create(name: "Example Item #3", trip_id: trip.id)
+
+puts "✅ Done seeding!"
+
